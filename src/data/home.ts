946509bd9519e75cd92c0c30f48e2f10c1d32822
esther-v <esvalas@gmail.com>
2024-01-14 15:44:34 +0100

    add categories section

---
+++ src/data/home.ts
@@ -0,0 +1,14 @@
+export const categories = [
+  "All",
+  "JavaScript",
+  "TypeScript",
+  "Programming",
+  "Weight Lifting",
+  "Bowling",
+  "Hiking",
+  "Travel",
+  "Electro music",
+  "React",
+  "Functional Programming",
+  "Coding",
+]
--- src/layouts/PageHeader.tsx
+++ src/layouts/PageHeader.tsx
@@ -1,11 +1,14 @@
-import { Bell, Menu, Mic, Search, Upload, User } from "lucide-react";
+import { ArrowLeft, Bell, Menu, Mic, Search, Upload, User } from "lucide-react";
 import logo from "../assets/react.svg";
 import { Button } from "../components/Button";
+import { useState } from "react";
 
 export function PageHeader() {
+  const [showFullWidthSearch, setShowFullWidthSearch] = useState(false)
+  console.log(showFullWidthSearch)
   return (
     <div className="flex gap-10 lg:gap-20 justify-between pt-2 mb-6 mx-4">
-      <div className="flex gap-4 items-center flex-shrink-0">
+      <div className={`gap-4 items-center flex-shrink-0 ${showFullWidthSearch ? "hidden" : "flex"}`}>
         <Button variant="ghost" size="icon">
           <Menu />
         </Button>
@@ -14,10 +17,15 @@ export function PageHeader() {
           U-tube
         </a>
       </div>
-      <form className="flex gap-4 flex-grow justify-center">
+      <form className={`gap-4 flex-grow justify-center ${showFullWidthSearch ? "flex" : "hidden md:flex "}`}>
+        {showFullWidthSearch && (
+          <Button onClick={() => setShowFullWidthSearch(false)}  type="button" size="icon" className="flex-shrink-0" variant="ghost">
+            <ArrowLeft />
+          </Button>
+        )}
         <div className="flex flex-grow max-w-[600px]">
           <input type="search" placeholder="Search" className="rounded-l-full border border-secondary-border py-1 px-4 text-lg w-full focus:border-blue-500 outline-none" />
-          <Button>
+          <Button className="py-2 px-4 rounded-r-full border-secondary-border border border-l-0 flex-shrink-0">
             <Search />
           </Button>
         </div>
@@ -25,7 +33,13 @@ export function PageHeader() {
           <Mic />
         </Button>
       </form>
-      <div className="flex flex-shrink-0 md:gap-2">
+      <div className={`flex-shrink-0 md:gap-2 ${showFullWidthSearch ? "hidden" : "flex"}`}>
+        <Button onClick={() => setShowFullWidthSearch(true)} variant="ghost" size="icon" className="md:hidden">
+          <Search />
+        </Button>
+        <Button variant="ghost" size="icon" className="md:hidden">
+          <Mic />
+        </Button>
         <Button variant="ghost" size="icon">
           <Upload />
         </Button>
